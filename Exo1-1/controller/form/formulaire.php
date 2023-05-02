0738aebaed04367b39adc2f8a3301b617cb03682
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-05-02 17:37:25 +0200

    form exo

--- Exo1-1/controller/form/formulaire.php
+++ Exo1-1/controller/form/formulaire.php
@@ -1,25 +1,10 @@
 
-
-
 <?php
-if(!empty($_POST))
-{
-// affichage des saisies pour être sur de les obtenir avant de les exploiter.
-echo "firstname : $_POST[firstname] <br>";
-echo "mdp : $_POST[password] <br>";
-echo "message : $_POST[message] <br>";
-// entête email
-$headers = 'MIME-Version: 1.0' . "\n";
+// Sanitization and validation of the form
+//--
 
-$headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n";
 
-$headers .= 'Reply-To: ' . $_POST['firstname'] . "\n";
+// if(!empty($_POST))
+// {
 
-$headers .= 'From: "' . ucfirst(substr($_POST['firstname'], 0, strpos($_POST['firstname'], '@'))) . '"<'.$_POST['firstname'].'>' . "\n";
-
-$headers .= 'Delivered-to: monadresse@gmail.com' . "\n";
-
-echo $headers;
-
-mail("monadresse@gmail.com", $_POST['firstname'], $_POST['message'], $headers);
-}
+// }
--- Exo1-1/index.php
+++ Exo1-1/index.php
@@ -4,3 +4,7 @@ include_once("view/view.php");
 include_once("model/model.php");
 include_once("controller/controller.php");
 
+
+
+$p = session_start();
+echo $p;
